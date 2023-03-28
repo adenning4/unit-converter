8bd469668e3a3bd387da6a53bb04efdbe6bbc0db
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-03-27 20:24:51 -0600

    js babysteps

---
+++ script.js
@@ -0,0 +1,7 @@
+//grab input value, this will be a string
+const inputVal = document.getElementById("input");
+const btnEl = document.getElementById("convert-btn");
+
+btnEl.addEventListener("click", function () {
+  console.log("yes");
+});
